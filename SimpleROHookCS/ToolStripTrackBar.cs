e50d7c3e7822991ae98974b8b06e7370c151bcab
sekishi1259 <sekishi1259@gmail.com>
2014-06-09 10:22:18 +0900

    add c# ToolStripTrackBar class
    add ToolStripTrackBar.cs

---
+++ SimpleROHookCS/ToolStripTrackBar.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+
+namespace SimpleROHookCS
+{
+    [ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.MenuStrip |
+                                   ToolStripItemDesignerAvailability.ContextMenuStrip)]
+    public class ToolStripTrackBar : ToolStripControlHost
+    {
+        //private TrackBar trackBar;
+
+        public ToolStripTrackBar() : base(new TrackBar()) { }
+
+        public TrackBar trackBar
+        {
+            get
+            {
+                return Control as TrackBar;
+            }
+        }
+
+        // Add properties, events etc. you want to expose...
+        public void SetMinMax(int min,int max)
+        {
+            trackBar.Minimum = min;
+            trackBar.Maximum = max;
+        }
+        public void SetTickFrequency(int tick)
+        {
+            trackBar.TickFrequency = tick;
+        }
+        public int Value 
+        {
+            get {
+                return trackBar.Value;
+            }
+            set {
+                trackBar.Value = value;
+            }
+        }
+        public void SetChangeValue(int small,int large)
+        {
+            trackBar.SmallChange = small;
+            trackBar.LargeChange = large;
+        }
+
+        protected override void OnSubscribeControlEvents(Control c)
+        {
+
+        }
+    }
+}
